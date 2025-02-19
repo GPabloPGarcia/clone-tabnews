ff0fe32161af87c46e1ad68089c6958ebd5dd5af
Pablo Garcia <45627780+GPabloPGarcia@users.noreply.github.com>
2025-02-19 20:00:23 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Angela eu te amo meu amor!!! Se você me ama dá uma risadinha</h1>
+    return <h1>Angela eu te amo meu amor!!! Se você me ama dá uma risadinha &#x1F60E;</h1>
 }
 
 export default Home;
